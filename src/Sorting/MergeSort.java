7d52459e275d0785de00f5d20a00a8632c37a1bb
Krish-Ramoliya <krishramoliya434@gmail.com>
2024-07-27 23:04:53 +0530

    TwoSum-2 Leetcode

---
+++ src/Sorting/MergeSort.java
@@ -0,0 +1,21 @@
+package Sorting;
+
+public class MergeSort {
+
+    public static void main(String []args)
+    {
+        int nums[]={2,1,5,3,8,4};
+        MerSort(nums);
+    }
+
+    private static void MerSort(int[] nums) {
+
+      int start = 0;
+      int end = nums.length-1;
+        if(start<end){
+
+            int mid  = start +(end-start)/2;
+
+        }
+    }
+}
---
+++ src/TwoPointersMethod/Twosum.java
@@ -0,0 +1,49 @@
+package TwoPointersMethod;
+
+import java.util.Arrays;
+
+public class Twosum {
+
+    public static void main(String[] args)
+    {
+        int nums[]={2,7,11,15};
+        int target = 9;
+        two ob = new two();
+        int arr[] = ob.twoSum(nums,9);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+}
+class two {
+    public int[] twoSum(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start<=end)
+        {
+            if((nums[start]+nums[end])==target)
+            {
+                if(nums[start]<nums[end])
+                {
+                    return  new int[]{start+1,end+1};
+                }
+                else{
+                    return   new  int[]{end+1 , start+1};
+                }
+
+            }
+            if((nums[start]+nums[end])>target)
+            {
+                end--;
+            }
+            else{
+                start++;
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+}
